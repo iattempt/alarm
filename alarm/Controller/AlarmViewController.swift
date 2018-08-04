c78b94c349c7d7100e1d31c6fcaad000f2ecad4a
Ernest <iattempt.net@gmail.com>
2018-08-04 22:02:23 +0800

    Refactor, squashed commit of the following:
    commit 7b9a2fb2a29f754c715bf5f02904baff2241db2e
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Aug 4 21:58:29 2018 +0800
    
        Extract method
    
    commit 0174585bac58459f129512e409c21cdae0495d05
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Aug 4 21:58:21 2018 +0800
    
        Extract method
    
    commit 82d4bb933f2d288a052bc2999f3c268270fc04f3
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Aug 4 21:51:14 2018 +0800
    
        Remove unused code
    
    commit 2156cd2f3efe1a93bb4ea1794985c65be013ab69
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Aug 4 21:50:49 2018 +0800
    
        Reorganize code
    
    commit 4673a2413bf7a923d7711b292448b764f829bae8
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Aug 4 21:50:17 2018 +0800
    
        Remove unnecessary comment
    
    commit 5f8f95bd66fbb3d71a6312c067870722dcc4783b
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Aug 4 21:46:55 2018 +0800
    
        Remove unused file
    
    commit 551580064852185888d55485c6439f3690acf21d
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Aug 4 21:45:34 2018 +0800
    
        Remove unused code
    
    commit f81bead0aeab3948142ee13cb83c2045a173981d
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Aug 4 21:45:07 2018 +0800
    
        Refactor: remove magic numbers
    
    commit b222157f76a81cf02cae21be6b23804a8c22342a
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sat Aug 4 21:13:08 2018 +0800
    
        Refactor: use enum instead of magic string

--- alarm/Controller/AlarmViewController.swift
+++ alarm/Controller/AlarmViewController.swift
@@ -8,28 +8,54 @@
 
 import UIKit
 
-enum FilterStatus {
-    case All
-    case On
-    case Off
+enum FilterStatus : String {
+    case All = "all"
+    case On = "on"
+    case Off = "off"
+}
+
+struct FilterGroupIds {
+    static let All = [-1]
+    static let NotBelongs = [-2]
+    static var EnabledGroups : [Int] {
+        get {
+            var result = [Int]()
+            for group in Groups.instance().groups() {
+                if group.enabled {
+                    result.append(group.groupId)
+                }
+            }
+            return result
+        }
+    }
+    static var DisabledGroups : [Int] {
+        get {
+            var result = [Int]()
+            for group in Groups.instance().groups() {
+                if !group.enabled {
+                    result.append(group.groupId)
+                }
+            }
+            return result
+        }
+    }
 }
 
 class AlarmViewController: UIViewController {
     var alarms = Alarms.instance().alarms()
-    @IBOutlet weak var tableView: UITableView!
     var filter_status_type: FilterStatus = .On
-    // nil means all
-    // [-1] means non-group alarms
-    var filter_groups: [Int]? = nil
+    var filter_group_ids: [Int] = FilterGroupIds.All
+
+    @IBOutlet weak var tableView: UITableView!
 
     @IBAction func filter_status(_ sender: UISegmentedControl) {
         let string = sender.titleForSegment(at: sender.selectedSegmentIndex)!
         switch string.lowercased() {
-        case "all":
+        case FilterStatus.All.rawValue:
             filter_status_type = FilterStatus.All
-        case "on":
+        case FilterStatus.On.rawValue:
             filter_status_type = FilterStatus.On
-        case "off":
+        case FilterStatus.Off.rawValue:
             filter_status_type = FilterStatus.Off
         default:
             break
@@ -40,40 +66,29 @@ class AlarmViewController: UIViewController {
     @IBAction func filter_group(_ sender: UIBarButtonItem) {
         let alertController = UIAlertController(title: "Filter", message: "", preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "All", style: UIAlertActionStyle.destructive, handler: { (action) in
-            self.filter_groups = nil
+            self.filter_group_ids = FilterGroupIds.All
             self.refresh()
         }))
         alertController.addAction(UIAlertAction(title: "Not belongs group", style: UIAlertActionStyle.destructive, handler: { (action) in
-            self.filter_groups = [-1]
+            self.filter_group_ids = FilterGroupIds.NotBelongs
             self.refresh()
         }))
         alertController.addAction(UIAlertAction(title: "Enabled group", style: UIAlertActionStyle.destructive, handler: { (action) in
-            self.filter_groups = []
-            for group in Groups.instance().groups() {
-                if group.enabled {
-                    self.filter_groups?.append(group.groupId)
-                }
-            }
+            self.filter_group_ids = FilterGroupIds.EnabledGroups
             self.refresh()
         }))
         alertController.addAction(UIAlertAction(title: "Disabled group", style: UIAlertActionStyle.destructive, handler: { (action) in
-            self.filter_groups = []
-            for group in Groups.instance().groups() {
-                if !group.enabled {
-                    self.filter_groups?.append(group.groupId)
-                }
-            }
+            self.filter_group_ids = FilterGroupIds.DisabledGroups
             self.refresh()
         }))
         for group in Groups.instance().groups() {
             alertController.addAction(UIAlertAction(title: group.groupLabel, style: .default, handler: { (action) in
-                self.filter_groups = []
-                self.filter_groups?.append(group.groupId)
+                self.filter_group_ids = []
+                self.filter_group_ids.append(group.groupId)
                 self.refresh()
             }))
         }
-        alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: { (action) in
-        }))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: { (action) in }))
         self.present(alertController, animated: true) {}
     }
 
@@ -139,39 +154,38 @@ extension AlarmViewController: UITableViewDelegate,
     }
 
     func isMatchingFilterGroup(_ theAlarm: Alarm) -> Bool {
-        // nil means all
-        if let theFilterGroupIds = filter_groups {
-            // empty means non-group alarms
-            if theFilterGroupIds.count == 1 && theFilterGroupIds[0] == -1 {
-                if theAlarm.groupId != nil {
-                    return false
-                }
-            } else {
-                if let theGroupId = theAlarm.groupId {
-                    var matched = false
-                    for groupId in theFilterGroupIds {
-                        if theGroupId == groupId {
-                            matched = true
-                        }
+        if filter_group_ids == FilterGroupIds.All {
+            return true
+        } else if filter_group_ids == FilterGroupIds.NotBelongs && theAlarm.groupId == nil {
+            return true
+        } else {
+            if let theGroupId = theAlarm.groupId {
+                for filterGroupId in filter_group_ids {
+                    if theGroupId == filterGroupId {
+                        return true
                     }
-                    return matched
-                } else {
-                    return false
                 }
             }
         }
-        return true
+        return false
+    }
+
+    fileprivate func getSwitchButton(_ theAlarm: Alarm) -> UISwitch {
+        let switchButton = UISwitch()
+
+        switchButton.isOn = theAlarm.isEnabled()
+        switchButton.tag = theAlarm.alarmId
+        switchButton.isEnabled = !theAlarm.isGroupAlarm() || theAlarm.isGroupEnabled()
+        switchButton.addTarget(self, action: #selector(self.switchChanged(_:)), for: .valueChanged)
+
+        return switchButton
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Todo: the animation is not smooth
         let theAlarm = self.alarms[indexPath.row]
 
-        let switchButton = UISwitch()
-        switchButton.isOn = theAlarm.isEnabled()
-        switchButton.tag = theAlarm.alarmId
-        switchButton.isEnabled = !theAlarm.isGroupAlarm() || theAlarm.isGroupEnabled()
-        switchButton.addTarget(self, action: #selector(self.switchChanged(_:)), for: .valueChanged)
+        let switchButton = getSwitchButton(theAlarm)
 
         let cell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: nil)
         cell.textLabel?.text = Utility.dateFormatter.string(from: theAlarm.date)
@@ -192,8 +206,6 @@ extension AlarmViewController: UITableViewDelegate,
         SelectedAlarm = Alarms.instance().alarm(byId: alarmId!)
         if isEditing {
             performSegue(withIdentifier: SegueIdentifiers.EditAlarm, sender: self)
-        } else {
-//            performSegue(withIdentifier: "later", sender: self)
         }
     }
 
@@ -234,6 +246,10 @@ extension AlarmViewController {
             isMatchingFilterGroup(theAlarm)
     }
 
+    private func isNotOutOfBound(_ nearestTimeIndexPathRow : Int) {
+        return 0 <= nearestTimeIndexPathRow && nearestTimeIndexPathRow < self.alarms.count
+    }
+
     func scrollToNearestRowOfDate() {
         var nearestTimeIndexPathRow = 0
         var isAnyCellVisible = false
@@ -250,7 +266,7 @@ extension AlarmViewController {
         }
 
         // Scroll to the nearest row
-        if isAnyCellVisible && 0 <= nearestTimeIndexPathRow && nearestTimeIndexPathRow < self.alarms.count {
+        if isAnyCellVisible && isNotOutOfBound(nearestTimeIndexPathRow) {
             self.tableView.scrollToRow(at: IndexPath(row: nearestTimeIndexPathRow, section: 0), at: .top, animated: false)
         }
     }
