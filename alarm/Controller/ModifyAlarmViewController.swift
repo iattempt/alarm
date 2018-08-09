7c78b85be890bcc5510bff2b807f38cc46307aad
Ernest <iattempt.net@gmail.com>
2018-08-09 11:23:05 +0800

    Change placeholder

--- alarm/Controller/ModifyAlarmViewController.swift
+++ alarm/Controller/ModifyAlarmViewController.swift
@@ -167,7 +167,7 @@ extension ModifyAlarmViewController {
             } else {
                 AlarmIdProp = NextAlarmId
                 GroupIdProp = nil
-                LabelProp = "Group"
+                LabelProp = "Alarm"
                 SoundIdProp = nil
                 SoundNameProp = "none"
                 VibrateIdProp = nil
